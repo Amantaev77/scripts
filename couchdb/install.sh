cc312cb3c455f94e459e3760bb645e32b05b1550
Artem <75737050+Amantaev77@users.noreply.github.com>
2025-11-08 10:09:15 +0300

    Update install.sh

--- couchdb/install.sh
+++ couchdb/install.sh
@@ -11,7 +11,7 @@ echo -e "${YELLOW}=== Автоматическая установка CouchDB н
 
 # Параметры установки (измените при необходимости)
 ADMIN_USER="admin"
-ADMIN_PASSWORD="password"
+ADMIN_PASSWORD="${COUCHDB_PASSWORD:-password}"
 BIND_ADDRESS="0.0.0.0"
 BIND_PORT="5984"
 SETUP_MODE="standalone"  # или "clustered"
